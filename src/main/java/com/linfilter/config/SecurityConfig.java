89636fd65b992e76d43a61e6cd13333ed62b8076
jsamol <julia.samol@gmail.com>
2017-05-24 02:03:49 +0200

    Add basic security and social configurations

---
+++ src/main/java/com/linfilter/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.linfilter.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                    .and()
+                .formLogin()
+                    .loginPage("/login")
+                    .permitAll()
+                .and()
+                    .rememberMe();
+    }
+
+
+}
